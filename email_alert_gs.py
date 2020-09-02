42ad6cec7ecda7d086c4b1377818a4258e9028c3
tnabtaf <clements@galaxyproject.org>
2020-09-02 15:54:35 -0700

    Updated Google Scholar Alert to ignore new part with content-type of text/x-amp-html.
    Also greatly cleaned up state handling.

--- email_alert_gs.py
+++ email_alert_gs.py
@@ -34,7 +34,38 @@ class GSEmailAlert(email_alert.EmailAlert, html.parser.HTMLParser):
     # Now using divs instead. Change caused search string, and text in pub to
     # disappear.
 
+    # States
+    # Just starting; ignore everything before this
+    STATE_LOOKING_FOR_HTML_PART = "Looking for HTML Part"
+
+    # next important bit is anchor containing referencing pub title
+    STATE_LOOKING_FOR_TITLE_LINK = "Looking for title link"
+
+    # And then we are in the title link
+    STATE_IN_TITLE_LINK = "In title link"
+
+    # after url to referencing pub, the title of that pub is next
+    STATE_IN_TITLE_TEXT = "In title text"
+
+    # Title is followed by author list for referencing paper
+    STATE_IN_AUTHOR_LIST = "In author list"
+
+    # Sometimes there is an excerpt from the referencing pub.
+    STATE_TEXT_FROM_PUB_NEXT = "Text from pub next"
+
+    # and we have found that excerpt.  This is the last state for each
+    # referencing pub in the email.
+    STATE_IN_TEXT_FROM_PUB = "In text from pub"
+
+    # sometimes, the search string is at the bottom of the email.
+    STATE_IN_SEARCH = "In search"
+
+    # Final state
+    STATE_SEARCH_PROCESSED = "Search Processed"
+
     search_start_re = re.compile(r'(Scholar Alert: )|(\[ \()')
+    html_part_start_re = re.compile(
+        r'Content-Type: text/html; charset="UTF-8"', re.MULTILINE)
 
     def __init__(self, email):
 
@@ -50,21 +81,15 @@ class GSEmailAlert(email_alert.EmailAlert, html.parser.HTMLParser):
         self._current_pub = None
         self._current_pub_alert = None
 
-        self._in_search = False
-        self._search_processed = False
-        self._in_title_ink = False
-        self._in_title_text = False
-        self._in_author_list = False
-        self._text_from_pub_next = False
-        self._in_text_from_pub = False
+        self._state = GSEmailAlert.STATE_LOOKING_FOR_HTML_PART
 
         # process the HTML body text.
         self.feed(self._email_body_text)
 
         # If search was not in message body, then pull it from subject line
-        if not self._search_processed:
+        if not self._state == GSEmailAlert.STATE_SEARCH_PROCESSED:
             self.search += " " + self._alert.subject
-            self._search_processed = True
+            self._state = GSEmailAlert.STATE_SEARCH_PROCESSED
 
         return None
 
@@ -73,15 +98,24 @@ class GSEmailAlert(email_alert.EmailAlert, html.parser.HTMLParser):
     def handle_data(self, data):
 
         data = data.strip()
-        starting_search = GSEmailAlert.search_start_re.match(data)
-        if starting_search:
-            self.search += data
-            self._in_search = True
+        if data == "":
+            return(None)
 
-        elif self._in_search:
+        if self._state == GSEmailAlert.STATE_LOOKING_FOR_HTML_PART:
+            if GSEmailAlert.html_part_start_re.search(data):
+                # Ignore any parts until we get to text/html.
+                # Not ignoring them leads to duplicate entries.
+                self._state = GSEmailAlert.STATE_LOOKING_FOR_TITLE_LINK
+
+        elif (self._state == GSEmailAlert.STATE_LOOKING_FOR_TITLE_LINK
+            and GSEmailAlert.search_start_re.match(data)):
+            self.search += data
+            self._state = GSEmailAlert.STATE_IN_SEARCH
+
+        elif self._state == GSEmailAlert.STATE_IN_SEARCH:
             self.search += " " + data
 
-        elif self._in_title_text and data:
+        elif self._state == GSEmailAlert.STATE_IN_TITLE_TEXT:
             # sometimes we lose space between two parts of title.
             pub_title = self._current_pub.title
             if (pub_title and pub_title[-1] != " "):
@@ -89,7 +123,7 @@ class GSEmailAlert(email_alert.EmailAlert, html.parser.HTMLParser):
             pub_title += data
             self._current_pub.set_title(pub_title)
 
-        elif self._in_author_list and data:
+        elif self._state ==  GSEmailAlert.STATE_IN_AUTHOR_LIST:
             if self._current_pub.canonical_first_author:
                 canonical_first_author = (
                     self._current_pub.canonical_first_author)
@@ -105,22 +139,24 @@ class GSEmailAlert(email_alert.EmailAlert, html.parser.HTMLParser):
                 canonical_first_author)
             if len(parts) == 2:
                 self._current_pub.ref = parts[1]
-        elif self._in_text_from_pub and data:
+
+        elif self._state ==  GSEmailAlert.STATE_IN_TEXT_FROM_PUB:
             self._current_pub_alert.text_from_pub += data + " "
 
         return(None)
 
     def handle_starttag(self, tag, attrs):
 
-        if tag == "h3":
+        if (tag == "h3"
+            and self._state == GSEmailAlert.STATE_LOOKING_FOR_TITLE_LINK):
             # link to paper is shown in h3.
-            self._in_title_link = True
+            self._state =  GSEmailAlert.STATE_IN_TITLE_LINK
             self._current_pub = publication.Pub()
             self._current_pub_alert = pub_alert.PubAlert(
                 self._current_pub, self)
             self.pub_alerts.append(self._current_pub_alert)
 
-        elif tag == "a" and self._in_title_link:
+        elif tag == "a" and self._state == GSEmailAlert.STATE_IN_TITLE_LINK:
             full_url = attrs[0][1]
             url_args = full_url[full_url.find("?")+1:].split("&")
 
@@ -137,30 +173,26 @@ class GSEmailAlert(email_alert.EmailAlert, html.parser.HTMLParser):
             if not self._current_pub.url:
                 # Some URLs link directly to Google Scholar.
                 self._current_pub.url = full_url
-            self._in_title_link = False
-            self._in_title_text = True
+            self._state =  GSEmailAlert.STATE_IN_TITLE_TEXT
 
-        elif tag in ["font", "div"] and self._text_from_pub_next:
-            self._text_from_pub_next = False
-            self._in_text_from_pub = True
+        elif (tag in ["font", "div"]
+                  and self._state == GSEmailAlert.STATE_TEXT_FROM_PUB_NEXT):
+            self._state =  GSEmailAlert.STATE_IN_TEXT_FROM_PUB
             self._current_pub_alert.text_from_pub = ""
 
         return (None)
 
     def handle_endtag(self, tag):
 
-        if tag == "b" and self._in_search:
-            self._in_search = False
-            self._search_processed = True
-        elif tag == "a" and self._in_title_text:
-            self._in_title_text = False
-        elif tag == "h3":
-            self._in_author_list = True
-        elif tag == "div" and self._in_author_list:
-            self._in_author_list = False
-            self._text_from_pub_next = True
-        elif tag in ["font", "div"] and self._in_text_from_pub:
-            self._in_text_from_pub = False
+        if tag == "b" and self._state ==  GSEmailAlert.STATE_IN_SEARCH:
+            self._state = GSEmailAlert.STATE_SEARCH_PROCESSED
+        elif tag == "h3" and self._state == GSEmailAlert.STATE_IN_TITLE_TEXT:
+            self._state = GSEmailAlert.STATE_IN_AUTHOR_LIST
+        elif tag == "div" and self._state == GSEmailAlert.STATE_IN_AUTHOR_LIST:
+            self._state = GSEmailAlert.STATE_TEXT_FROM_PUB_NEXT
+        elif (tag in ["font", "div"]
+            and self._state == GSEmailAlert.STATE_IN_TEXT_FROM_PUB):
+            self._state = GSEmailAlert.STATE_LOOKING_FOR_TITLE_LINK
 
         return (None)
 
